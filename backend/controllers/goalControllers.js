dbcbd99332547f80091391c6f575c69aeccfc581
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-10-21 08:00:06 -0400

    Express Async Handler

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,18 +1,20 @@
+const asyncHandler = require('express-async-handler')
+
 /**
  * @desc    Get goals
  * @param   GET /api/goals
  * @access  Private 
  */
-const getGoals = (req, res) =>{
+const getGoals = asyncHandler(async (req, res) =>{
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 /**
  * @desc    Set goal
  * @param   POST /api/goals/:id
  * @access  Private 
  */
-const setGoal = (req, res) =>{
+const setGoal = asyncHandler(async (req, res) => {
     
     if (!req.body.text) {
         res.status(400)
@@ -20,25 +22,25 @@ const setGoal = (req, res) =>{
     }
 
     res.status(200).json({message: 'Set goal'})
-}
+})
 
 /**
  * @desc    Delete goal
  * @param   POST /api/goals/:id
  * @access  Private 
  */
-const deleteGoal = (req, res) =>{
+const deleteGoal = asyncHandler(async (req, res) =>{
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 /**
  * @desc    Update goal
  * @param   POST /api/goals/:id
  * @access  Private 
  */
-const updateGoal = (req, res) =>{
+const updateGoal = asyncHandler(async (req, res) =>{
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals, setGoal, deleteGoal, updateGoal,

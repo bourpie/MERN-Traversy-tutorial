776079de304d9df91cd67257982b70343df997bc
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-10-22 06:14:51 -0400

    models and CRUD

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,12 +1,17 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 /**
  * @desc    Get goals
  * @param   GET /api/goals
  * @access  Private 
  */
 const getGoals = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: 'Get goals'})
+
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 /**
@@ -21,16 +26,11 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set goal'})
-})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
 
-/**
- * @desc    Delete goal
- * @param   POST /api/goals/:id
- * @access  Private 
- */
-const deleteGoal = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    res.status(200).json({goal})
 })
 
 /**
@@ -39,10 +39,43 @@ const deleteGoal = asyncHandler(async (req, res) =>{
  * @access  Private 
  */
 const updateGoal = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
+})
+
+/**
+ * @desc    Delete goal
+ * @param   POST /api/goals/:id
+ * @access  Private 
+ */
+ const deleteGoal = asyncHandler(async (req, res) =>{
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await Goal.remove
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
-    getGoals, setGoal, deleteGoal, updateGoal,
+    getGoals, 
+    setGoal, 
+    deleteGoal, 
+    updateGoal,
 }
 

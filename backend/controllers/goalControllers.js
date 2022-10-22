f12f26d3a9708ebcfab20fc97fb22ef3dbffb001
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-10-22 10:12:32 -0400

    Check user on delete or update

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 /**
  * @desc    Get goals
@@ -9,7 +10,7 @@ const Goal = require('../models/goalModel')
  */
 const getGoals = asyncHandler(async (req, res) =>{
 
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user:  req.user.id })
 
     res.status(200).json(goals)
 })
@@ -27,7 +28,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
 
     res.status(200).json({goal})
@@ -46,6 +48,20 @@ const updateGoal = asyncHandler(async (req, res) =>{
         throw new Error('Goal not found')
     }
     
+    const user = await User.findById(req.user.id)
+
+    // Check user
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
         new: true,
     })
@@ -65,6 +81,24 @@ const deleteGoal = asyncHandler(async (req, res) => {
       res.status(400)
       throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+
+    // Check user
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not athorized')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+        new: true,
+    })
 
     await goal.remove()
   

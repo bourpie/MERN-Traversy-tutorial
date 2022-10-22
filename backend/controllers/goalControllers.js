43094e6a29e30c694b880494c3c601f02704576e
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-10-22 06:47:19 -0400

    fix delete

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -58,24 +58,23 @@ const updateGoal = asyncHandler(async (req, res) =>{
  * @param   POST /api/goals/:id
  * @access  Private 
  */
- const deleteGoal = asyncHandler(async (req, res) =>{
-
+const deleteGoal = asyncHandler(async (req, res) => {
     const goal = await Goal.findById(req.params.id)
-
+  
     if (!goal) {
-        res.status(400)
-        throw new Error('Goal not found')
+      res.status(400)
+      throw new Error('Goal not found')
     }
 
-    await Goal.remove
-
-    res.status(200).json({id: req.params.id})
-})
+    await goal.remove()
+  
+    res.status(200).json({ id: req.params.id })
+  })
 
 module.exports = {
     getGoals, 
-    setGoal, 
+    setGoal,
+    updateGoal, 
     deleteGoal, 
-    updateGoal,
 }
 
